2f452fa37554f323a3f07499b497838c0421afae
ApamateSoft <jealmesa@gmail.com>
2023-04-19 21:43:56 -0400

    add test

--- test/rule.test.ts
+++ test/rule.test.ts
@@ -1,22 +1,42 @@
 import {describe, expect, test} from '@jest/globals';
 import ValidatorBuilder from "../src/ValidatorBuilder";
 
-describe('sum module', () => {
+describe('Rule function test', () => {
 
-  test('adds 1 + 2 to equal 3', () => {
+  test('The onInvalidEvaluation event must be executed if the validation is not met', () => {
+
+    const errorMessage = 'error message';
+    const evaluate = '';
+
+    const validate = (evaluate: string) => evaluate.length > 0;
+
+    const handleInvalidEvaluation = jest.fn( (message: string) => {} )
 
     const validator = new ValidatorBuilder()
-      .rule('Hola mundo', evaluate => evaluate.length > 0)
-      .addOnInvalidEvaluation( message => console.log(message) )
+      .rule(errorMessage, validate)
+      .addOnInvalidEvaluation(handleInvalidEvaluation)
       .build();
-    validator.isValid('')
+    validator.isValid(evaluate);
 
-    // const validator = new Validator();
-    // validator.rule('Hola mundo', evaluate => evaluate.length > 0 );
-    // validator.addOnInvalidEvaluation(message => console.log(message));
-    // validator.isValid('');
+    expect(handleInvalidEvaluation).toHaveBeenCalled()
+  });
 
-    expect(3).toBe(3);
+  test('The onInvalidEvaluation event should not be executed if the validation is fulfilled', () => {
+
+    const errorMessage = 'error message';
+    const evaluate = 'a';
+
+    const validate = (evaluate: string) => evaluate.length > 0;
+
+    const handleInvalidEvaluation = jest.fn( (message: string) => {} )
+
+    const validator = new ValidatorBuilder()
+      .rule(errorMessage, validate)
+      .addOnInvalidEvaluation(handleInvalidEvaluation)
+      .build();
+    validator.isValid(evaluate);
+
+    expect(handleInvalidEvaluation).not.toHaveBeenCalled()
   });
 
 });
